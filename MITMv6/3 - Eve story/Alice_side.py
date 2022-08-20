8347df840a4651c5278d922390d608663432568c
SimchaTeich <sim.teich@gmail.com>
2022-08-21 00:30:24 +0300

    short the scripts

--- MITMv6/3 - Eve story/Alice_side.py
+++ MITMv6/3 - Eve story/Alice_side.py
@@ -4,38 +4,25 @@ import socket
 import time
 
 KB = 1024
-
-"""
-BOB: Alice, I finally managed the ping6.
-
-ALICE: really? how
-
-BOB: My network card was in a promiscuous state, so I canceled it
-
-ALICE: Sounds good. But now something strange is happening to me...
-
-BOB: What do you mean strange?
-
-ALICE: I think Eve is playing cache on me. again.
-
-Bob: I mean she's listening to us now?!
-"""
+PORT = 13332
+HOST = "fe80::1eaa:7821:6306:2979%enp0s3"
+ADDR = HOST, PORT
+ALICE_MSGS = ["really? how",
+              "Sounds good. But now something strange is happening to me...",
+              "I think Eve is playing cache on me. again."]
 
 def main():
-    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    sock.create_connection("fe80::1eaa:7821:6306:2979%enp0s3", 13337))
+    sock = socket.create_connection(ADDR)
    
-    print(sock.recv(KB).decode())
-    sock.sendall(b"really? how")
-    time.sleep(2)
-    print(sock.recv(KB).decode())
-    sock.sendall(b"Sounds good. But now something strange is happening to me...")
-    time.sleep(2)
-    print(sock.recv(KB).decode())
-    sock.sendall(b"I think Eve is playing cache on me. again.")
-    time.sleep(2)
-    print(sock.recv(KB).decode())
-    time.sleep(2)
+    for msg in ALICE_MSGS:
+        time.sleep(2)
+        print("Bob: " + sock.recv(KB).decode())
+        time.sleep(2)
+        print("Me: " + msg)
+        sock.sendall(msg.encode())
+    
+    while True:
+        pass
     
     sock.close()
     
--- MITMv6/3 - Eve story/Bob_side.py
+++ MITMv6/3 - Eve story/Bob_side.py
@@ -4,40 +4,29 @@ import socket
 import time
 
 KB = 1024
-
-"""
-BOB: Alice, I finally managed the ping6.
-
-ALICE: really? how
-
-BOB: My network card was in a promiscuous state, so I canceled it
-
-ALICE: Sounds good. But now something strange is happening to me...
-
-BOB: What do you mean strange?
-
-ALICE: I think Eve is playing cache on me. again.
-
-Bob: I mean she's listening to us now?!
-"""
+PORT = 13332
+HOST = "::"
+ADDR = HOST, PORT
+BOB_MSGS = ["Alice, I finally managed the ping6.",
+            "My network card was in a promiscuous state, so I canceled it",
+            "What do you mean strange?",
+            "I mean she's listening to us now?!"]
 
 def main():
     listen_sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    listen_sock.bind(("::", 13337))
+    listen_sock.bind(ADDR)
     listen_sock.listen(1)
     client_sock, _ = listen_sock.accept()
     
-    client_sock.sendall(b"Alice, I finally managed the ping6.")
-    time.sleep(2)
-    print(client_sock.recv(KB).decode())
-    client_sock.sendall(b"My network card was in a promiscuous state, so I canceled it")
-    time.sleep(2)
-    print(client_sock.recv(KB).decode())
-    client_sock.sendall(b"What do you mean strange?")
-    time.sleep(2)
-    print(client_sock.recv(KB).decode())
-    client_sock.sendall(b"I mean she's listening to us now?!")
-    time.sleep(2)
+    for msg in BOB_MSGS:
+        time.sleep(2)
+        print("Me: " + msg)
+        client_sock.sendall(msg.encode())
+        time.sleep(2)
+        print("Alice: " + client_sock.recv(KB).decode())
+    
+    while True:
+        pass
     
     client_sock.close()
     listen_sock.close()
